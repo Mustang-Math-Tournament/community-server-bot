0773f4a90b83a5f00148b330b9e8d60f7230356b
Vince7778 <realvince7778@gmail.com>
2022-06-03 18:19:05 -0700

    Completely rewrite Command (breaks everything)

--- src/Command.ts
+++ src/Command.ts
@@ -1,113 +1,102 @@
 
-// Class file for the Command class.
-// A Command stores the information for commands and can execute them.
-// Commands are case insensitive.
+// See https://discord.com/developers/docs/interactions/application-commands#subcommands-and-subcommand-groups for how the layout works.
 
-import { Message } from "discord.js";
 import { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
 
-type AnyCommandBuilder = SlashCommandBuilder | SlashCommandSubcommandBuilder | SlashCommandSubcommandGroupBuilder;
-
-// TODO: use generics to determine if subcommand
 interface CommandOptions {
-    name: string; // Name of the command.
-    description: string; // Description of the command.
-    subcommands?: Command[]; // Subcommands for this command.
-    isSubcommand?: boolean; // Whether or not this command is a subcommand. Default false.
-
-    // Function that is executed when command is called and it doesn't match a subcommand.
-    // msg is the Message object of the sent command, text is the remaining part of the command.
-    // Example: for the command "!test hello 123", text will be "hello 123"
-    exec: (msg: Message, text: string) => any;
-
-    needsArgs?: boolean; // Whether or not this command needs arguments (text is not empty). Default false.
-
-    listens?: boolean; // Whether or not this command listens to all sent messages. Default false.
-    listenExec?: (msg: Message) => any; // Function that executes on every message while listening.
-
-    buildSlash: () => AnyCommandBuilder; // Function that builds a slash command. Name, description, and subcommands are automatically set.
+    subcommands?: Subcommand[];
+    subcommandGroups?: SubcommandGroup[];
+    exec?: (inter: CommandInteraction) => void; // Function to execute without subcommands
+    slash: SlashCommandBuilder; // The slash command representing this command. This must already have subcommands+groups added to it.
 }
 
 export class Command {
-    name: string;
-    description: string;
-    subcommands: Command[];
-    isSubcommand: boolean;
-    exec: (msg: Message, text: string) => any;
-    needsArgs: boolean;
-    listens: boolean;
-    listenExec: (msg: Message) => any;
-    buildSlash: () => AnyCommandBuilder;
+    subcommands: Subcommand[];
+    subcommandGroups: SubcommandGroup[];
+    exec?: (inter: CommandInteraction) => void; // this is ignored if there are subcommands
+    slash: SlashCommandBuilder;
 
     constructor(opts: CommandOptions) {
-        this.name = opts.name;
-        this.description = opts.description;
-        this.subcommands = opts.subcommands ?? [];
-        this.isSubcommand = opts.isSubcommand ?? false;
+        this.slash = opts.slash;
         this.exec = opts.exec;
-        this.needsArgs = opts.needsArgs ?? false;
-        this.listens = opts.listens ?? false;
-        this.listenExec = opts.listenExec ?? (()=>{});
-        this.buildSlash = opts.buildSlash;
+        this.subcommands = opts.subcommands ?? [];
+        this.subcommandGroups = opts.subcommandGroups ?? [];
     }
 
-    // Checks if text is a valid form of this command.
-    matchAlias(text: string): boolean {
-        if (text === "") return false;
-        const firstWord = text.split(" ")[0];
-        return this.name === firstWord.toLowerCase();
-    }
+    checkExecute(inter: CommandInteraction) {
+        if (inter.commandName !== this.slash.name) return;
 
-    // Checks for matching subcommands, and executes the command if none found.
-    // text can still include the command alias.
-    checkExecute(msg: Message, text: string) {
-        // remove command word
-        let splitText = text.split(" ");
-        if (splitText.length > 0 && this.name === splitText[0].toLowerCase()) {
-            splitText = splitText.splice(1);
-        }
-        let newText = splitText.join(" ");
-
-        let foundSubcommand = false;
-        for (const subcommand of this.subcommands) {
-            if (subcommand.matchAlias(newText)) {
-                subcommand.checkExecute(msg, newText);
-                foundSubcommand = true;
-                break;
-            }
-        }
-
-        if (!foundSubcommand) {
-            if (this.needsArgs && newText.length === 0) {
-                msg.channel.send("Error: you must include arguments for this command.");
-            } else {
-                this.exec(msg, newText);
-            }
-        }
-    }
-
-    // Executes the listen function when receiving command, and propagates it to the subcommands.
-    checkListen(msg: Message) {
-        for (const subcommand of this.subcommands) {
-            subcommand.checkListen(msg);
-        }
-        if (this.listens) this.listenExec(msg);
-    }
-
-    // Get the full slash command including name + description + subcommands.
-    getSlash() {
-        const built = this.buildSlash();
-        built.setName(this.name).setDescription(this.description);
-
-        if (built instanceof SlashCommandBuilder) { // not a subcommand
-            for (const subcommand of this.subcommands) {
-                const subSlash = subcommand.getSlash();
-                if (subSlash instanceof SlashCommandSubcommandBuilder) {
-                    built.addSubcommand(subSlash);
+        const subcommandGroupName = inter.options.getSubcommandGroup();
+        if (subcommandGroupName) {
+            for (const cmdGroup of this.subcommandGroups) {
+                if (cmdGroup.slash.name === subcommandGroupName) {
+                    cmdGroup.checkExecute(inter);
+                    return;
                 }
             }
+            throw "No subcommand group with name "+subcommandGroupName;
         }
-        
-        return built;
+
+        const subcommandName = inter.options.getSubcommand();
+        if (subcommandName) {
+            for (const cmd of this.subcommands) {
+                if (cmd.slash.name === subcommandName) {
+                    cmd.exec(inter);
+                    return;
+                }
+            }
+            throw "No subcommand with name "+subcommandName;
+        }
+
+        if (!this.exec) {
+            throw "No execute function on command "+this.slash.name;
+        }
+
+        this.exec(inter);
+    }
+}
+
+interface SubcommandOptions {
+    exec: (inter: CommandInteraction) => void;
+    slash: SlashCommandSubcommandBuilder;
+}
+
+export class Subcommand {
+    exec: (inter: CommandInteraction) => void;
+    slash: SlashCommandSubcommandBuilder;
+
+    constructor(opts: SubcommandOptions) {
+        this.slash = opts.slash;
+        this.exec = opts.exec ?? (() => {});
+    }
+}
+
+interface SubcommandGroupOptions {
+    slash: SlashCommandSubcommandGroupBuilder;
+    subcommands: Subcommand[];
+}
+
+export class SubcommandGroup {
+    subcommands: Subcommand[];
+    slash: SlashCommandSubcommandGroupBuilder;
+
+    constructor(opts: SubcommandGroupOptions) {
+        this.slash = opts.slash;
+        this.subcommands = opts.subcommands;
+    }
+
+    checkExecute(inter: CommandInteraction) {
+        const subcommandName = inter.options.getSubcommand();
+        if (subcommandName) {
+            for (const cmd of this.subcommands) {
+                if (cmd.slash.name === subcommandName) {
+                    cmd.exec(inter);
+                    return;
+                }
+            }
+            throw "No subcommand with name "+subcommandName+" in group "+this.slash.name;
+        }
+        throw "Somehow trying to execute a subcommand group";
     }
 }
