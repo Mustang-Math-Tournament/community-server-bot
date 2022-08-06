94c216cd12ba485f073503848e18702ab866c9c8
Eesha-Jain <eesha.jain801@gmail.com>
2022-08-06 14:58:51 -0700

    attempt 2

--- src/commands/problem/edit/question.ts
+++ src/commands/problem/edit/question.ts
@@ -6,7 +6,8 @@ import { CommandInteraction } from "discord.js";
 import { verifyAdmin } from "../../../checkPermissions";
 import { Subcommand } from "../../../Command";
 import { getProblem, getUnfinished } from "../../../stores/problemQueue";
-import { typeset } from "../../../util/latex.js";
+import katex from "katex";
+import puppeteer from "puppeteer";
 
 async function exec(inter: CommandInteraction) {
     if (!verifyAdmin(inter, true)) return;
@@ -25,7 +26,69 @@ async function exec(inter: CommandInteraction) {
     }
 
     const tex = inter.options.getString("question");
-    await typeset(tex).then(async (image: any) => {
+    const htmlLatex = katex.renderToString(tex ?? "", {
+        throwOnError: false,
+        output: "html"
+    });
+
+    const html = `
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous">
+
+            <!-- The loading of KaTeX is deferred to speed up page rendering -->
+            <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.js" integrity="sha384-X/XCfMm41VSsqRNQgDerQczD69XqmjOOOwYQvr/uuC+j4OPoNhVgjdGFwhvN02Ja" crossorigin="anonymous"></script>
+
+            <!-- To automatically render math in text elements, include the auto-render extension: -->
+            <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
+                onload="renderMathInElement(document.body);"></script>
+
+            <style>
+                body {
+                    height: max-content;
+                    color: white;
+                    font-size: 30px;
+                }
+                
+                .katex-display > .katex { white-space: pre; }
+                .katex-display > .base { margin: 0.25em 0; }
+                .katex-display { margin: 0.5em 0; }
+
+                .katex-display .katex !important {
+                    white-space: inherit;
+                }
+            </style>
+        </head>
+
+        <body>
+        ${htmlLatex}
+        </body>
+    </html>
+    `;
+
+    console.log(htmlLatex);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(html);
+
+    const content = await page.$("body");
+    if (content != null) {
+        const imageBuffer = await content.screenshot({ omitBackground: true });
+
+        await page.close();
+        await browser.close();
+
+        await inter.reply({
+            files: [{
+                attachment: imageBuffer,
+                name: "problem.png"
+            }]
+        });
+    }
+
+     /* await typeset(tex).then(async (image: any) => {
         await inter.reply({
             files: [{
                 attachment: image,
@@ -36,7 +99,7 @@ async function exec(inter: CommandInteraction) {
         await inter.reply(err);
     });
 
-    /* const newQuestion = inter.options.getString("question");
+   const newQuestion = inter.options.getString("question");
     if (!newQuestion) {
         const oldQuestion = problemObj.question;
         problemObj.question = "";
