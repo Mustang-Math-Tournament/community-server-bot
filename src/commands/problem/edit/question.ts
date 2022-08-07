5b2d0876d8eaa93f30dccd8a6915651a50b58a9c
Eesha-Jain <eesha.jain801@gmail.com>
2022-08-07 11:49:14 -0700

    version 3

--- src/commands/problem/edit/question.ts
+++ src/commands/problem/edit/question.ts
@@ -6,11 +6,12 @@ import { CommandInteraction } from "discord.js";
 import { verifyAdmin } from "../../../checkPermissions";
 import { Subcommand } from "../../../Command";
 import { getProblem, getUnfinished } from "../../../stores/problemQueue";
-import katex from "katex";
 import puppeteer from "puppeteer";
 
 async function exec(inter: CommandInteraction) {
     if (!verifyAdmin(inter, true)) return;
+
+    inter.reply({ content: "Processing..." });
 
     const problemId = inter.options.getInteger("problemid", true);
     const problemObj = getUnfinished(problemId);
@@ -21,94 +22,66 @@ async function exec(inter: CommandInteraction) {
         } else {
             content = "No problems exist with this id.";
         }
-        await inter.reply({ content, ephemeral: true });
+        await inter.channel?.send({ content });
         return;
     }
 
-    const tex = inter.options.getString("question");
-    const htmlLatex = katex.renderToString(tex ?? "", {
-        throwOnError: false,
-        output: "html"
-    });
-
+    // const tex = inter.options.getString("question");
     const html = `
     <html>
         <head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous">
-
-            <!-- The loading of KaTeX is deferred to speed up page rendering -->
-            <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.js" integrity="sha384-X/XCfMm41VSsqRNQgDerQczD69XqmjOOOwYQvr/uuC+j4OPoNhVgjdGFwhvN02Ja" crossorigin="anonymous"></script>
-
-            <!-- To automatically render math in text elements, include the auto-render extension: -->
-            <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
-                onload="renderMathInElement(document.body);"></script>
-
             <style>
                 body {
                     height: max-content;
                     color: white;
-                    font-size: 30px;
-                }
-                
-                .katex-display > .katex { white-space: pre; }
-                .katex-display > .base { margin: 0.25em 0; }
-                .katex-display { margin: 0.5em 0; }
-
-                .katex-display .katex !important {
-                    white-space: inherit;
+                    font-size: 40px;
+                    width: 900px;
+                    max-width: 900px; border: solid;
                 }
             </style>
         </head>
 
         <body>
-        ${htmlLatex}
+        <script type="text/x-mathjax-config">
+            MathJax.Hub.Config({
+                CommonHTML: { linebreaks: { automatic: true } },
+                "HTML-CSS": { linebreaks: { automatic: true } },
+                SVG: { linebreaks: { automatic: true } }
+            });
+            window.addEventListener('resize', MJrerender);
+            function MJrerender(){
+            MathJax.Hub.Queue(["Rerender",MathJax.Hub])
+            };
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG-full"></script>
+
+        <h1>Problem Of The Day</h1>
+        <p id="example">$$a_1 + a_2 + a_3 + a_4 + a_5 + a_6 + a_7 + a_8 + a_9 + a_{10} + a_{11} + a_{12} + a_{13} + a_{14} + a_{15} + a_{16} + a_{17} + a_{18} + a_{19} + a_{20}$$</p>
         </body>
     </html>
     `;
 
-    console.log(htmlLatex);
-
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.waitForTimeout(1000);
 
-    await page.setContent(html);
-
+    await page.goto(`data:text/html,${html}`);
     const content = await page.$("body");
+    await page.waitForTimeout(1000);
+
     if (content != null) {
         const imageBuffer = await content.screenshot({ omitBackground: true });
 
         await page.close();
         await browser.close();
 
-        await inter.reply({
+        await inter.channel?.send({
             files: [{
                 attachment: imageBuffer,
                 name: "problem.png"
             }]
         });
     }
-
-     /* await typeset(tex).then(async (image: any) => {
-        await inter.reply({
-            files: [{
-                attachment: image,
-                name: "problem.png"
-            }]
-        });
-    }).catch(async (err: any) => {
-        await inter.reply(err);
-    });
-
-   const newQuestion = inter.options.getString("question");
-    if (!newQuestion) {
-        const oldQuestion = problemObj.question;
-        problemObj.question = "";
-        const readQuestion = oldQuestion ? `:\n${oldQuestion}` : " blank.";
-        await inter.reply(`Problem ${problemId}'s question was erased. It previously was${readQuestion}`);
-    } else {
-        problemObj.question = newQuestion;
-        await inter.reply(`Problem ${problemId}'s question was updated to:\n${newQuestion}`);
-    }*/
 }
 
 const slash = new SlashCommandSubcommandBuilder()
